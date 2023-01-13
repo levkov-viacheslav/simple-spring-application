c240f726a6f1ae7a90a66bdd7421bcc5b90b6ac0
Levkov Viacheslav <mtlevkov@gmail.com>
2023-01-13 13:10:07 +0200

    add app files

---
+++ app/src/main/java/levkov/Person.java
@@ -0,0 +1,21 @@
+package levkov;
+
+public class Person {
+    private String name;
+
+    public Person(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void sayHello() {
+        System.out.println(this.name + " says: \"Hello!\"");
+    }
+}
